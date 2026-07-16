6efbf16de720e70eba19a3ca1590a60efa74c5fe
Alpha1221-gif <ishan21780@gmail.com>
2026-07-16 20:51:56 +0530

    end

---
+++ OUTPUT/Output.py
@@ -0,0 +1,38 @@
+#The most basic function for display a output is:(print)
+
+# ----CODE1----     
+print("Your name ") #output = Your name
+
+# ----CODE2----(For multiple lines)
+print("Your name") #output = Your name
+print("I want to learn a python!") #output = I want to learn a python!
+print("I am very happy") #output = I am very happy
+
+# ----CODE3----(By using single quotes)
+print("Your name")
+print('Your name')
+'''Both statement gives the same output without any error'''
+
+# ----CODE4----(By using without any quotes)
+print(Your name) #output = SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+
+# ----CODE5---(If you want the two prints line print the statement in one single line then use <end=" ">)
+print("Your name",end = " ")
+print("I want to learn a python!")
+#output = Your name I want to learn a python!
+
+# ----CODE6----(If you want to print numeric or integer value)
+print(23) #output = 23
+print(45) #output = 45
+print(678) #output = 678
+
+# ----CODE7----(Using a add,sub etc calculations) 
+print(89+45) #output = 134
+print(89-45) #output = 44
+print(89*45) #output = 4005
+print(89/45) #output = 1.97
+
+# ----CODE8----(Mixed code using a text or integer no..)
+print("My mother is",40,"years old")
+#output = My mother is 40 years old
