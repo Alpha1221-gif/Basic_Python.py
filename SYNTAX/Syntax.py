9685df0ec191d0d4186e80403739dd6cb68f8400
Alpha1221-gif <ishan21780@gmail.com>
2026-07-16 15:51:03 +0530

    Syntax in python

--- SYNTAX/Syntax.py
+++ SYNTAX/Syntax.py
@@ -1,2 +1,33 @@
-#Syntax in a python is play a very major role for ex..
+# ----CODE 1----
 print("hello world")
+#output = hello world
+
+# ----CODE2----
+'''About proper indentation'''
+if(34>7):
+    print("thirty four is greater than seven") 
+   #OUTPUT = thirty four is greater than seven
+'''IN this you give the space of about four or one (without this the error is come)'''  
+
+# ----CODE3----
+if 34 > 7:
+ print("thirty four is greater than seven") 
+if 34 > 7:
+        print("thirty four is greater than seven")
+#output = thirty four is greater than seven
+         #thirty four is greater than seven
+'''In this if you write directly second print statement without indentation you face error.
+The another way to write a another print statement is to write with left corner without indentation'''
+
+if 34 > 7:
+ print("thirty four is greater than seven") 
+print("thirty four is greater than seven")
+#output = same as above code
+
+# ----CODE4----
+print("Hello Brother!")
+print("It is a cat.")
+print("I an learning a python!")
+
+'''The another way to write this code in a same line are (but we use a (;) <semicolon>in this)'''
+print("Hello Brother!");print("It is a cat.");print("I an learning a python!")
