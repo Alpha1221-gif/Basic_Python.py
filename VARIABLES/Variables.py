3677a498079d5ad969c204e4eafaee5adb529d37
Alpha1221-gif <ishan21780@gmail.com>
2026-07-17 16:11:44 +0530

    variables

---
+++ VARIABLES/Variables.py
@@ -0,0 +1,55 @@
+# ----CODE1----
+x = 3
+y = "Alice"
+print(x) #output = 3 
+print(y) #output = Alice
+
+# ----CODE2----
+x = 5
+y = 5
+z = 5
+print(int(x)) #output = 5
+print(float(y)) #output = 5.0
+print(str(z)) #output = '5' 
+
+# ----CODE3----
+x = 3
+y = "Alice"
+print(type(x)) #output = <class 'int'>
+print(type(y)) #output = <class 'str'>
+
+# ----CODE4----
+x = 3
+X = "Barny"
+print(x) #in python x is not equal X (x!=X)
+
+# ----CODE5----
+a,b,c = "Apple","Mango","Banana"
+print(a) #output = Apple
+print(b) #output = Mango
+print(c) #output = Banana
+
+# ----CODE6----
+a=b=c = "Apple"
+print(a) #output = Apple
+print(b) #output = Apple
+print(c) #output = Apple
+
+# ----CODE7----
+fruits = ["apple","Mango", "banana"]
+x, y, z = fruits
+print(x) #output = apple
+print(y) #output = Mango
+print(z) #output = banana
+
+# ----CODE8----
+x = "Python"
+y = "is"
+z = "amazing"
+print(x, y, z) #output = Python is amazing
+
+# ----CODE9----
+x = "Python "
+y = "is "
+z = "amazing"
+print(x + y + z) #output = Python is amazing
