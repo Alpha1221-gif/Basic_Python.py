261f7cd1e9df784a0bcb01b70d128b3f77d7a92b
Alpha1221-gif <ishan21780@gmail.com>
2026-07-17 20:49:32 +0530

    new changes

---
+++ DATA_TYPES/Data_types.py
@@ -0,0 +1,59 @@
+# ----CODE1----
+a = "Hello World"
+print(type(a)) #output = <class 'str'>
+
+# ----CODE2----
+b = 30
+print(type(b)) #output = <class 'int'>
+
+# ----CODE3----
+c = 39.5
+print(type(c)) #output = <class 'float'>
+
+# ----CODE4----
+d = 1j
+print(type(d)) #output = <class 'complex'>
+
+# ----CODE5----
+e = ["yellow","orange","green","purple"]
+print(type(e)) #output = <class 'list'>
+
+# ----CODE6----
+f = ("yellow","orange","green","purple")
+print(type(f)) #output = <class 'tuple'>
+
+# ----CODE7----
+g = range(7)
+print(type(g)) #output = <class 'range'>
+
+# ----CODE8----
+h = {"Hero":"Iron man","Age":53}
+print(type(h)) #output = <class 'dict'>
+
+# ----CODE9----
+i = {"yellow","orange","green","purple"}
+print(type(i)) #output = <class 'set'>
+
+# ----CODE10----
+j = frozenset({"yellow","orange","green","purple"})
+print(type(j)) #output = <class 'frozenset'>
+
+# ----CODE11----
+k = False
+print(type(k)) #output = <class 'bool'>
+
+# ----CODE12----
+l = b"Hey"
+print(type(l)) #output = <class 'bytes'>
+
+# ----CODE13----
+m = bytearray(35)
+print(type(m)) #output = <class 'bytearray'>
+
+# ----CODE14----
+n = memoryview(bytes(35))
+print(type(n)) #output = <class 'memoryview'>
+
+# ----CODE15----
+o = None
+print(type(o)) #output = <class 'NoneType'>
