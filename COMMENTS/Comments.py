119c61065701a9b82a049a2fa14183cca1b126e6
Alpha1221-gif <ishan21780@gmail.com>
2026-07-17 15:37:10 +0530

    end of comments

---
+++ COMMENTS/Comments.py
@@ -0,0 +1,11 @@
+# ----CODE1----
+#Code for print a name(used as a comment to describe about your code)
+print("Alice")
+
+# ----CODE2----
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
