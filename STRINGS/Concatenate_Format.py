5144b70bab23627695d099cdb438b614afd2c1bd
Alpha1221-gif <ishan21780@gmail.com>
2026-07-19 12:38:40 +0530

    new change

---
+++ STRINGS/Concatenate_Format.py
@@ -0,0 +1,21 @@
+# ----CODE1----(Concatenate)
+a = "Hello"
+b = "World"
+c = a + b
+print(c) #output = HelloWorld
+
+# ----CODE2----(Concatenate)
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c) #output = Hello World
+
+# ----CODE3----(Format)
+age = 46
+txt = f"My name is Alice, I am {age} years old."
+print(txt) #output = My name is Alice, I am 46 years old.
+
+# ----CODE4----(Format)
+price = 69
+txt = f"The price is {price:.2f} dollars without Discount."
+print(txt) #output = The price is 69.00 dollars without Discount.
