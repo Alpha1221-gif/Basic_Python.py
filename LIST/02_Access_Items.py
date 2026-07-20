dfadba33f1ddb632aa768aeee184bc5c759010da
Alpha1221-gif <ishan21780@gmail.com>
2026-07-20 14:37:29 +0530

    python change

---
+++ LIST/02_Access_Items.py
@@ -0,0 +1,39 @@
+# ----CODE1---- #
+list = ["Iron man", "Hulk", "Doctor strange"]
+print(list)
+# output = ['Iron man', 'Hulk', 'Doctor strange']
+
+# ----CODE2---- #
+list = ["Iron man", "Hulk", "Doctor strange","Hulk"]
+print(list)
+# output = ['Iron man', 'Hulk', 'Doctor strange','Hulk']
+
+# ----CODE3---- #
+list = ["Iron man", "Hulk", "Doctor strange","Hulk"]
+print(len(list))
+# output = 4
+
+# ----CODE4---- #
+list = ["Iron man", "Hulk", "Doctor strange"]
+print(list[0])
+# output = Iron man
+
+# ----CODE5---- #
+list = ["Iron man", "Hulk", "Doctor strange"]
+print(list[-1])
+# output = Doctor strange
+
+# ----CODE6---- #
+list = ["Iron man", "Hulk", "Doctor strange","Captian America","Black panther","Ant man","Rocket"]
+print(list[2:5])
+# output = ['Doctor strange', 'Captian America', 'Black panther']
+
+# ----CODE7---- #
+list = ["Iron man", "Hulk", "Doctor strange","Captian America","Black panther","Ant man","Rocket"]
+print(list[:5])
+# output = ['Iron man', 'Hulk', 'Doctor strange', 'Captian America', 'Black panther']
+
+# ----CODE8---- #
+list = ["Iron man", "Hulk", "Doctor strange","Captian America","Black panther","Ant man","Rocket"]
+print(list[2:])
+# output = ['Doctor strange', 'Captian America', 'Black panther', 'Ant man', 'Rocket']
---
+++ LIST/03_Change_Items.py
@@ -0,0 +1,11 @@
+# ----CODE1---- #
+list = ["Iron man", "Hulk", "Doctor strange","Captian America","Black panther","Ant man","Rocket"]
+list[1] = "Thor"
+print(list)
+# output = ['Iron man', 'Thor', 'Doctor strange', 'Captian America', 'Black panther', 'Ant man', 'Rocket']
+
+# ----CODE2---- #
+list = ["Iron man", "Hulk", "Doctor strange","Captian America","Black panther","Ant man","Rocket"]
+list.insert(3,"Vision")
+print(list)
+# output = ['Iron man', 'Hulk', 'Doctor strange', 'Vision', 'Captian America', 'Black panther', 'Ant man', 'Rocket']
