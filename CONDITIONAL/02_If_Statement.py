b9c21b57572afee018a009a64d0587738dc8f1b1
Alpha1221-gif <ishan21780@gmail.com>
2026-07-21 16:18:22 +0530

    commit

---
+++ CONDITIONAL/02_If_Statement.py
@@ -0,0 +1,28 @@
+# 1. Concept: Floor division (//) divides and rounds down to the nearest whole number
+x = 15
+y = 4
+result = x // y  # result is 3
+
+# 2. Equals condition
+if result == 3:
+    print("Equals: The result is 3")
+
+# 3. Not Equals condition
+if result != 5:
+    print("Not Equals: The result is not 5")
+
+# 4. Less than condition
+if result < 5:
+    print("Less than: The result is less than 5")
+
+# 5. Less than or equal to condition
+if result <= 3:
+    print("Less than or equal to: The result is 3 or less")
+
+# 6. Greater than condition
+if result > 2:
+    print("Greater than: The result is greater than 2")
+
+# 7. Greater than or equal to condition
+if result >= 3:
+    print("Greater than or equal to: The result is 3 or more")
