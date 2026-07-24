e2ef08a3aee0a3416f44b7a2aff43f2d8de4f8b3
Alpha1221-gif <ishan21780@gmail.com>
2026-07-24 21:01:42 +0530

    arrays

---
+++ ARRAYS/Arrays.py
@@ -0,0 +1,22 @@
+# ----CODE1----
+cars = ["Mercedes","BMW","Toyata"]
+a = cars[0]
+print(a) #output = Mercedes
+
+# ----CODE2----
+cars = ["Mercedes","BMW","Toyata"]
+b = len(cars)
+print(b) #output = 3
+
+#----CODE3----
+cars = ["Mercedes","BMW","Toyata"]
+for x in cars:
+ print(x)
+ '''Mercedes
+BMW
+Toyata''' 
+
+# ----CODE4----
+cars = ["Mercedes","BMW","Toyata"]
+cars.append("Honda")
+print(cars) #output = ['Mercedes', 'BMW', 'Toyata', 'Honda']
