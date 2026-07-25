3b731700765f6a0ff7b669df56a08680bea4512b
Alpha1221-gif <ishan21780@gmail.com>
2026-07-25 21:03:49 +0530

    Maths in python

---
+++ MATH/Math.py
@@ -0,0 +1,32 @@
+# ----CODE1----
+a = min(23,45,67)
+b = max(34,56,32)
+
+print(a) #output = 23
+print(b) #output = 56
+
+# ----CODE2----
+c = abs(-23) #Returns the +ve value as a output
+print(c) #output = 23
+
+# ----CODE3----
+d = pow(2,3) #Returns the value as (a,b) -> a^b
+print(d) #output = 8
+
+# ----CODE4----
+import math
+e = math.sqrt(81) #Returns the square root of the no.
+print(e)  #output = 9.0
+
+# ----CODE5----
+import math
+f = math.ceil(2.5) #rounds a number upwards to its nearest integer
+g = math.floor(2.5) #rounds a number downwards to its nearest integer
+
+print(f) #output = 3
+print(g) #output = 2
+
+# ----CODE6----
+import math
+h = math.pi #returns the value of π
+print(h) #output = 3.141592653589793
